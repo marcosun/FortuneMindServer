190ae6a9c20b75ddf3b7db629c785d49cc630287
marcosun <marcoinbristol@gmail.com>
2017-03-26 16:09:17 +0800

    fund company type CRUD api

---
+++ models/fundCompanyType.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const FundCompanyTypeSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+}, {
+    timestamps: true,
+});
+
+export const FundCompanyTypeModel = mongoose.model('fund.company.type', FundCompanyTypeSchema);
+
+export default FundCompanyTypeModel;
---
+++ routes/fundCompanyType.js
@@ -0,0 +1,88 @@
+import FundCompanyTypeModel from '../models/fundCompanyType';
+
+export function getFundCompanyType (req, res, next) {
+    
+    FundCompanyTypeModel.find()
+        .select('name -_id')
+        .exec((err, fundCompanyTypes) => {
+        
+            if (err) {
+                res.status(500).send(err);
+                return next();
+            }
+
+            return res.status(200).send(fundCompanyTypes);
+            
+    });
+    
+};
+
+export function postFundCompanyType (req, res, next) {
+    
+    const data = req.body;
+    
+    const newFundCompanyType = {
+        name: data.name,
+    };
+    
+    FundCompanyTypeModel.create(newFundCompanyType, (err) => {
+
+            if (err) {
+                
+                switch (err.code) {
+                    case 11000:
+                        res.status(403).send({errMsg: '类型已经存在'});
+                        break;
+                    default:
+                        res.status(500).send(err);
+                }
+                
+                return next();
+            }
+
+            return res.status(200).send({msg: '创建成功'});
+
+    });
+    
+};
+
+export function putFundCompanyType (req, res, next) {
+    
+    const oldName = req.body.oldName;
+    const name = req.body.name;
+    
+    FundCompanyTypeModel.findOneAndUpdate({name: oldName}, {name})
+        .exec((err) => {
+
+            if (err) {
+                res.status(403).send(err);
+                return next();
+            }
+
+            return res.status(200).send({msg: '修改成功'});
+
+    });
+    
+};
+
+export function deleteFundCompanyType (req, res, next) {
+    
+    const name = req.body.name;
+    
+    FundCompanyTypeModel.findOneAndRemove({name})
+        .exec((err, fundCompanyType) => {
+
+            if (err) {
+                res.status(403).send(err);
+                return next();
+            }
+            
+            if (fundCompanyType) {
+                return res.status(200).send({msg: '删除成功'});
+            } else {
+                return res.status(403).send({errMsg: '类型不存在'});
+            }
+
+    });
+    
+};
