cea515e1a1a9108a619f403c88a46882a31b1808
marcosun <marcoinbristol@gmail.com>
2017-04-07 17:38:45 +0800

    only admin can access to admin apis

--- index.prod.js
+++ index.prod.js
@@ -2,7 +2,7 @@
 import path from 'path';
 import express from 'express';
 import logger from 'morgan';
-import cors from 'cors';
+
 import bodyParser from 'body-parser';
 import methodOverride from 'method-override';
 import cookieParser from 'cookie-parser';
@@ -16,12 +16,13 @@ let app = express();
 
 app.set('port', 18110);
 app.use(logger('combined'));
-app.use(cors());
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('X-HTTP-Method-Override'));
 app.use(cookieParser());
-app.use(cookieSession({secret: 'the princess and the frog', name: 'session',keys: ['key1', 'key2']}));
+//three days auto login
+app.use(cookieSession({ secret: 'britannia rule the wave', maxAge: 3 * 24 * 60 * 60 * 1000, }));
 
 // Configure passport middleware
 app.use(passport.initialize());
@@ -49,12 +50,13 @@ app.use('/dist', express.static('dist'));
 app.use('/public', express.static('public'));
 app.use('/tmp', express.static('tmp'));
 
+import { ensureAdmin } from './utils/utils';
 // Register routes
 import userRoute from './routes/user';
 import adminRoute from './routes/admin';
 
 app.use('/api/v1/user', userRoute);
-app.use('/api/v1/admin', adminRoute);
+app.use('/api/v1/admin', ensureAdmin, adminRoute);
 
 app.listen(app.get('port'), function(){
   console.log(("Express server listening on port " + app.get('port')))
